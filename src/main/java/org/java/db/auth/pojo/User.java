4a2a125d99e0d8c9c068a2d84991b1d02a734509
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-10-15 17:08:05 +0200

    [Add]ManyToMany User_Role

--- src/main/java/org/java/db/auth/pojo/User.java
+++ src/main/java/org/java/db/auth/pojo/User.java
@@ -1,9 +1,15 @@
 package org.java.db.auth.pojo;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
@@ -13,7 +19,7 @@ public class User {
 	@GeneratedValue()
 	private int id;
 	
-	@Column(nullable = false)
+	@Column(nullable = false , unique = true)
 	@NotNull
 	private String username;
 
@@ -21,11 +27,15 @@ public class User {
 	@NotNull
 	private String password;
 	
+	@ManyToMany(fetch = FetchType .EAGER)
+	private Set<Role> roles;
+	
 	public User() {};
-	public User(String username , String password) {
+	public User(String username , String password, Role... roles) {
 		
 		setUsername(username);
 		setPassword(password);
+		setRoles(new HashSet<>(Arrays.asList(roles)));
 	}
 	
 	public int getId() {
@@ -52,9 +62,34 @@ public class User {
 		this.password = password;
 	}
 	
+	//
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
 	@Override
 	public String toString() {
 
 		return "id : " + getId() + "->" + getUsername();
+	}
+	
+	@Override
+	public int hashCode() {
+		
+		return getId();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (!(obj instanceof Role)) return false;
+		
+		Role objRole = (Role) obj;
+		
+		return getId() == objRole.getId();
 	}
 }
